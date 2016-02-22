c10c734c4d3d8561a1c2ccc0ff3441da00f54131
raduwen <raduwen@gmail.com>
2016-02-22 21:03:29 +0900

    implements convert

--- web.rb
+++ web.rb
@@ -1,5 +1,26 @@
+require 'kconv'
+require 'jkf'
 require 'sinatra'
+if development?
+  require 'sinatra/reloader'
+  require 'pry'
+end
 
 get '/' do
-  'hello'
+  erb :index
+end
+
+post '/convert' do
+  str = params['kifu-file'][:tempfile].read.toutf8
+  jkf = Jkf.parse(str)
+  converter = case params['convert-type']
+              when 'ki2'
+                Jkf::Converter::Ki2.new
+              when 'kif'
+                Jkf::Converter::Kif.new
+              when 'csa'
+                Jkf::Converter::Csa.new
+              end
+  @result = converter.convert(jkf)
+  erb :index
 end
